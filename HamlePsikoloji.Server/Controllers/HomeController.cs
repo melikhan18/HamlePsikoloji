eaa3d85b284537edcdcdab666e9303bc4ec5db2d
melikhan <melikhan@DESKTOP-4TVV5I4>
2024-01-20 03:01:10 +0300

    Add project files.

---
+++ HamlePsikoloji.Server/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HamlePsikoloji.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet(Name = "GetString")]
+        public String Get()
+        {
+            return "Deneme123";
+        }
+
+
+        [HttpPost(Name = "GetString")]
+        public String Post()
+        {
+            return "Deneme123";
+        }
+    }
+}
